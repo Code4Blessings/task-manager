70991313c9382f687b3b6839ffbca259d1353830
Code4Blessings <robincw35@gmail.com>
2021-12-17 05:58:42 -0500

    Installed mongodb and created mongodb.js

---
+++ mongodb.js
@@ -0,0 +1,15 @@
+//crud Create Read Update Delete
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionUrl = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
+    if(error) {
+        console.log('Unable to connect to database!')
+    }
+    const db = client.db(databaseName);
+    
+})

ddb09522cef5973e5b97a6f7cbf5fab270ac2706
Code4Blessings <robincw35@gmail.com>
2021-12-18 11:14:46 -0500

    Moved mongodb lecture and assignment to ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # task-manager
+
+## Lecture - Inserting Documents
+
+     db.collection('users').insertOne({
+         name: 'Christine',
+         age: 37
+     }, (error, result) => {
+         if(error) {
+             return console.log('Unable to insert user')
+         }
+         console.log(result.insertedId)
+    })
+    db.collection('users').insertMany([
+         {
+             name: 'Robin',
+             age: 47
+         },
+         {
+            name: 'Daniel',
+            age: 13
+        }
+    ], (error, result) => {
+        if(error) {
+            return console.log('Unable to insert users')
+        }
+        console.log(result.insertedIds)
+    })
+
+## Assignment
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Wash the dishes',
+            completed: true
+        },
+        {
+            description: 'Cook dinner',
+            completed: false
+        },
+        {
+            description: 'Do the laundry',
+            completed: false
+        }
+    ], (error, result) => {
+        if(error) {
+            return console.log('Unable to insert task')
+        }
+        console.log(result.insertedIds)
+    })
--- mongodb.js
+++ mongodb.js
@@ -1,62 +1,25 @@
 //crud Create Read Update Delete
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
-const ObjectId = mongodb.ObjectId
+
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectId = mongodb.ObjectId
+
+//Destructured way to write the constants up above
+const { MongoClient, ObjectId } = require('mongodb');
 
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectId()
+console.log(id)
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
     if(error) {
         console.log('Unable to connect to database!')
     }
     const db = client.db(databaseName);
-    //Lecture - Inserting Documents
-
-    // db.collection('users').insertOne({
-    //     name: 'Robin',
-    //     age: 47
-    // }, (error, result) => {
-    //     if(error) {
-    //         return console.log('Unable to insert user')
-    //     }
-    //     console.log(result.insertedId)
-    // })
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Robin',
-    //         age: 47
-    //     },
-    //     {
-    //         name: 'Daniel',
-    //         age: 13
-    //     }
-    // ], (error, result) => {
-    //     if(error) {
-    //         return console.log('Unable to insert users')
-    //     }
-    //     console.log(result.insertedIds)
-    // })
-    //Assignment
-
-    db.collection('tasks').insertMany([
-        {
-            description: 'Wash the dishes',
-            completed: true
-        },
-        {
-            description: 'Cook dinner',
-            completed: false
-        },
-        {
-            description: 'Do the laundry',
-            completed: false
-        }
-    ], (error, result) => {
-        if(error) {
-            return console.log('Unable to insert task')
-        }
-        console.log(result.insertedIds)
-    })
+    
+    
 })

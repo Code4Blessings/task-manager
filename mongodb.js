895ffa0835516153f7d03315816ceee0c3afa220
Code4Blessings <robincw35@gmail.com>
2021-12-21 22:39:34 -0500

    Completed Query assignment

--- mongodb.js
+++ mongodb.js
@@ -19,7 +19,24 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
     //     }
     //     console.log(user)
     // })
-    db.collection('users').find({age: 47}).toArray((error, users) => {
-        console.log(users);
+    // db.collection('users').find({age: 47}).toArray((error, users) => {
+    //     console.log(users);
+    // })
+    //Goal: Use find and findone with tasks
+    //1. Use findOne to fetch the last task by id (print doc to console)
+    //2. Use find to fetch all the tasks that are not completed (print docs to console)
+    //3. Test your work
+    db.collection('tasks').findOne({_id: new ObjectId("61bdec5f9a1bd9769e915f08")}, (error, task) => {
+        if(error) {
+            return console.log('Unable to fetch task')
+        }
+        console.log(task)
+    })
+    db.collection('tasks').find({completed: false}).toArray((error, task) => {
+        if(error) {
+            return console.log('Unable to fetch tasks')
+        }
+        console.log(task)
     })
 })
+

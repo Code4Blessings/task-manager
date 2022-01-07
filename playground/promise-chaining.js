0845f1fe229f7dd462829449d9d176cb1e4d1121
Code4Blessings <robincw35@gmail.com>
2022-01-07 10:10:58 -0500

    Updated promise-chaining file

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -12,3 +12,14 @@ User.findByIdAndUpdate('61ccd56c3b11615d8d83f630', {age: 1}).then((user) => {
     console.log(e)
 })
 
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('61ccd56c3b11615d8d83f630', 2).then((count) => {
+    console.log(count)
+}).catch((e) => {
+    console.log(e)
+})

0319e3fe6f88c1d652a7c1fb03795fcfb7299a54
Code4Blessings <robincw35@gmail.com>
2022-07-22 06:28:35 -0400

    Fixed login bug

--- src/models/user.js
+++ src/models/user.js
@@ -10,10 +10,10 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         default: 0,
         required: true,
         trim: true,
-        lowercase: true,
         validate(value) {
             if(!validator.isEmail(value)) {
                 throw new Error('Email is invalid')
@@ -42,6 +42,21 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email: email })
+
+    if(!user) {
+        throw new Error('Unable to login')
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+    
+    if(!isMatch){
+        throw new Error('Unable to login')
+    }
+    return user
+}
+
+//Hash the plain text password before saving
 //Arrow functions don't work here
 userSchema.pre('save', async function(next) {
     const user = this
--- src/routers/user.js
+++ src/routers/user.js
@@ -18,11 +18,22 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        console.log(user)
+        res.send(user)
+    }catch(e) {
+        res.status(400).send()
+    }
+})
+
 router.get('/', async (req, res) => {
     try {
         const users = await User.find({})
         res.send(users)
     }catch(e) {
+        console.log(e)
         res.status(404).send()
     }
 })

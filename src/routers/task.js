6e9c35d3e35c5b31c485a7a91a0e1caddb97ef23
Code4Blessings <robincw35@gmail.com>
2022-08-19 08:03:48 -0400

    Added a sortBy method to the get tasks route

--- src/routers/task.js
+++ src/routers/task.js
@@ -23,12 +23,24 @@ router.post('/', auth, async (req, res) => {
 
 //limit allows you to limit the amount of data on each page
 //skip allows you to iterate over pages.  If the limit is 10 and skip is 0, then I get the first 10 results. If my limit is 10 and I skip 10 then I get the next 10 results on the second page.  If I skip 20, I get the 10 results on the third page.
+//SortBy
+//descending is 1, and ascending is -1
+//completed: -1 will put the completed tasks on the top and the incompletes obn the bottom. 1 will give vice versa
 //GET /tasks?completed=false
 //GET /tasks?limit=10&skip=10
+//GET /tasks?sortBy=createdAt:desc
+// desc for descending and asc for ascending
 router.get('/', auth, async (req, res) => {
     const match = {}
+    const sort = {}
     if(req.query.completed) {
         match.completed = req.query.completed === 'true'
+    }
+    if(req.query.sortBy) {
+        //The variable "parts" will be an array
+        const parts = req.query.sortBy.split(':')
+        //Instead of doing sort.completed or sort.createdAt, use whatever is first in the parts array. The below states that in the parts array, the first value is equal to 1 of 2 conditions. If it is equal to 'desc' then apply -1 (because that is how you define desc, asc equals 1). This will make sortBy dynamic to whatever query is written in that regard.
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
     try {
        await req.user.populate({
@@ -37,7 +49,8 @@ router.get('/', auth, async (req, res) => {
         options: {
             //parseInt - allows us to parse a string that contains a number into an actual integer
             limit: parseInt(req.query.limit),
-            skip: parseInt(req.query.skip)
+            skip: parseInt(req.query.skip),
+            sort
         }
        })
          res.send(req.user.tasks)

104774bd95335ba8130768f14cc0ca6607af5387
Code4Blessings <robincw35@gmail.com>
2022-09-04 05:44:06 -0400

    Installed multer pkg for file uploads

--- src/index.js
+++ src/index.js
@@ -6,21 +6,15 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-//Middleware must be above our app.use calls
+const multer =require('multer')
+const upload = multer({
+    dest: 'images'
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
 
-// app.use((req, res, next) => {
-//     if(req.method === 'GET') {
-//         res.send('Get requests are disabled')
-//     }else {
-//         next()
-//     }
-// })
 
-// app.use((req, res, next) => {
-//     if(req.method === 'GET' || req.method === 'POST' || req.method === 'PATCH' || req.method === 'DELETE'){
-//         res.status(503).send('Site temporaily down for maintenance and will return shortly')
-//     }
-// })
 
 app.use(express.json())
 app.use('/users', userRouter)
@@ -32,17 +26,3 @@ app.listen(port, () => {
     console.log('Server is listening on port ' + port)
 })
 
-const Task = require('./models/task')
-const User = require('./models/user')
-
-// const main = async () => {
-//     // const task = await Task.findById('62e5b8a11410e9d011e6d382')
-//     // await task.populate('owner')//.execPopulate()
-//     // console.log(task.owner)
-
-//     const user = await User.findById('62e52a2fede733978e88f046')
-//     await user.populate('tasks')
-//     console.log(user.tasks)
-// }
-
-// main()
